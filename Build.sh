ebdfb81f502829f7a3a5224f174fb24c77665cdd
Hiroki Tagato <tagattie@gmail.com>
2017-10-02 22:18:09 +0900

    Don't parallelize jobs when target is other than buildworld or buildkernel.

--- Build.sh
+++ Build.sh
@@ -19,13 +19,10 @@ if [ "${7}" != "null" ]; then
     ADD_ARGS=${7}
 fi
 
-shift 7
-TARGETS=$@
-
 NJOBS=$(sysctl hw.ncpu|awk '{print $NF}')
 
 MAKE_ENVS="MAKEOBJDIRPREFIX=${OBJDIRPREFIX}"
-MAKE_FLAGS="-j ${NJOBS} -DDB_FROM_SRC -DNO_FSCHG"
+MAKE_FLAGS="-DDB_FROM_SRC -DNO_FSCHG"
 MAKE_ARGS="TARGET=${ARCH_m} TARGET_ARCH=${ARCH_p} ${KERN_ARGS} ${DEST_ARGS} ${ADD_ARGS}"
 MAKE_TARGET="${TARGETS}"
 
@@ -34,6 +31,14 @@ if [ "${dryRunBuild}" == "true" ]; then
 fi
 MAKE_FLAGS="${DRYRUN_FLAG} ${MAKE_FLAGS}"
 
-BUILD_COMMAND="env ${MAKE_ENVS} make ${MAKE_FLAGS} ${MAKE_ARGS} ${MAKE_TARGET}"
-
-(cd ${SRCDIR} && ${BUILD_COMMAND})
+shift 7
+for i in "$@"; do
+    if [ "${i}" == "buildworld" ] || \
+           [ "${i}" == "buildkernel" ]; then
+        FINAL_MAKE_FLAGS="-j ${NJOBS} ${MAKE_FLAGS}"
+    else
+        FINAL_MAKE_FLAGS="${MAKE_FLAGS}"
+    fi
+    BUILD_COMMAND="env ${MAKE_ENVS} make ${FINAL_MAKE_FLAGS} ${MAKE_ARGS} ${i}"
+    (cd ${SRCDIR} && ${BUILD_COMMAND})
+done

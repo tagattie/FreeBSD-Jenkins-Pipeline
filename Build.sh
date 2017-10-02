ef533f6042890a2ef77be591c607974dc3f74913
Hiroki Tagato <tagattie@gmail.com>
2017-10-02 17:24:20 +0900

    Take kernel config, destination dir, and other arguments.

--- Build.sh
+++ Build.sh
@@ -9,14 +9,23 @@ SRCDIR=${1}
 OBJDIRPREFIX=${2}
 ARCH_m=${3}
 ARCH_p=${4}
-TARGET=${5}
+if [ -n "${5}" ]; then
+    KERN_ARGS="KERNCONF=${5}"
+fi
+if [ -n "${6}" ]; then
+    DEST_ARGS="DESTDIR=${6}"
+fi
+ADD_ARGS=${7}
+
+shift 7
+TARGETS=$@
 
 NJOBS=$(sysctl hw.ncpu|awk '{print $NF}')
 
 MAKE_ENVS="MAKEOBJDIRPREFIX=${OBJDIRPREFIX}"
 MAKE_FLAGS="-j ${NJOBS} -DDB_FROM_SRC -DNO_FSCHG"
-MAKE_ARGS="TARGET=${ARCH_m} TARGET_ARCH=${ARCH_p}"
-MAKE_TARGET="${TARGET}"
+MAKE_ARGS="TARGET=${ARCH_m} TARGET_ARCH=${ARCH_p} ${KERN_ARGS} ${DEST_ARGS} ${ADD_ARGS}"
+MAKE_TARGET="${TARGETS}"
 
 if [ "${dryRunBuild}" == "true" ]; then
     DRYRUN_FLAG="-n"

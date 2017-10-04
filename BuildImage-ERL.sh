ad81b5636fe0e9d3cb6f83daa767f1d1f92ab45e
Hiroki Tagato <tagattie@gmail.com>
2017-10-04 21:59:56 +0900

    - Rename script since it is just for ERL. - Various bug fixes.

--- BuildImage-ERL.sh
+++ BuildImage-ERL.sh
@@ -20,10 +20,11 @@ WORKDIR="$(pwd)/work"
 IMAGEFILE="FreeBSD-${BRANCHNAME}-${ARCHNAME}-${HOSTNAME}-${BUILDNAME}.img"
 
 KiB=1024
-MiB=$((1024*1024))
+MiB=$((${KiB}*1024))
 GiB=$((${MiB}*1024))
 # Image size is 2GiB
-IMGSIZE="$((2*${GiB}))"
+IMGSIZE=$((2*${GiB}))
+IMGSIZEMB=$((${IMGSIZE}/${MiB}))
 # Very traditional logical disk structure
 SECTORSPERTRACK=63
 HEADSPERCYLINDER=255
@@ -33,6 +34,7 @@ STARTSECTOR=63
 PARTSCHEME="MBR"
 # Boot partition
 BOOTPARTSIZE=$((256*${MiB}))
+BOOTPARTSIZEMB=$((${BOOTPARTSIZE}/${MiB}))
 BOOTPARTTYPE='!12'              # FAT32 (LBA-addressable)
 BOOTPARTLABEL="msdosboot"
 BOOTPARTFSTYPE="msdosfs"
@@ -44,56 +46,61 @@ FBPARTFSTYPE="freebsd-ufs"
 FBPARTFSALIGN=$((64*${KiB}))
 FBPARTFSENDIAN="big"
 FBPARTFSLABEL="rootfs"
-FBPARTFSMINFREE=$(echo $((2*${GiB}/1024*0.5)) | sed -e 's/\.*$//')
-FBPARTFSSIZE=$(((1024-256)*${MiB}))
+FBPARTFSMINFREE=$(bc -e "2*${GiB}/1024*0.5" -e quit | awk -F'.' '{print $1}')
+FBPARTFSMARGIN=$((64*${MiB}))
+FBPARTFSSIZE=$(((${IMGSIZEMB}-${BOOTPARTSIZEMB})*${MiB}-${FBPARTFSMARGIN}))
 
 mkdir -p "${WORKDIR}"
-cd "${WORKDIR}"
 
-truncate -s "${IMGSIZE}" "${IMAGEFILE}"
+truncate -s "${IMGSIZE}" "${WORKDIR}/${IMAGEFILE}"
 MDDEVNAME=$(${SUDO_COMMAND} mdconfig -a -t vnode \
-    -x ${SECTORSPERTRACK} -y ${HEADSPERCYLINDER} -f ${IMAGEFILE})
+    -x ${SECTORSPERTRACK} -y ${HEADSPERCYLINDER} -f ${WORKDIR}/${IMAGEFILE})
 echo "Memory disk device name is ${MDDEVNAME}"
 
 # Create partition scheme
-gpart create -s "${PARTSCHEME}" "${MDDEVNAME}"
+"${SUDO_COMMAND}" gpart create -s "${PARTSCHEME}" "${MDDEVNAME}"
 # Create FAT32(LBA-addressable) partition for boot (1st slice)
-gpart add -a ${SECTORSPERTRACK} -b ${STARTSECTOR} \
+"${SUDO_COMMAND}" gpart add -a "${SECTORSPERTRACK}" \
+    -b "${STARTSECTOR}" \
     -s $((${BOOTPARTSIZE}/${BYTESPERSECTOR})) \
-    -t '!12' ${MDDEVNAME}
+    -t "${BOOTPARTTYPE}" "${MDDEVNAME}"
 # Make the slice (boot slice) active (bootable)
-gpart set -a active -i 1 "${MDDEVNAME}"
+"${SUDO_COMMAND}" gpart set -a active \
+    -i 1 "${MDDEVNAME}"
 # Create FAT16 filesystem in the boot slice
-"${SUDO_COMMAND}" newfs_msdos \
-    -L "${BOOTPARTLABEL}" \
-    -F "${BOOTPARTFS}" "/dev/${MDDEVNAME}"
+"${SUDO_COMMAND}" newfs_msdos -L "${BOOTPARTLABEL}" \
+    -F "${BOOTPARTFS}" "/dev/${MDDEVNAME}s1"
 # Mount the FAT16 filesystem and copy necessary files onto it
 mkdir -p "${WORKDIR}/${BOOTPARTLABEL}"
 "${SUDO_COMMAND}" mount -t "${BOOTPARTFSTYPE}" \
-    -l "/dev/${MDDEVNAME}" "${WORKDIR}/${BOOTPARTLABEL}"
+    -l "/dev/${MDDEVNAME}s1" "${WORKDIR}/${BOOTPARTLABEL}"
 rsync -a --info=STATS3 --delete --stats \
     "${DESTDIR}/boot/kernel" \
     "${WORKDIR}/${BOOTPARTLABEL}"
-${SUDO_COMMAND} umount "${WORKDIR}/${BOOTPARTLABEL}"
+"${SUDO_COMMAND}" umount "${WORKDIR}/${BOOTPARTLABEL}"
 rmdir "${WORKDIR}/${BOOTPARTLABEL}"
 
 # Create FreeBSD partition (2nd slice)
-gpart add -t "${FBPARTTYPE}" "${MDDEVNAME}"
+"${SUDO_COMMAND}" gpart add -t "${FBPARTTYPE}" "${MDDEVNAME}"
 # Set partition scheme of the FreeBSD slice
-gpart create -s "${FBPARTSCHEME}" "${MDDEVNAME}s2"
+"${SUDO_COMMAND}" gpart create -s "${FBPARTSCHEME}" "${MDDEVNAME}s2"
 # Add freebsd-ufs partition (make alignment to 64KiB)
-gpart add -a "${FBPARTFSALIGN}" -t "${FBPARTFSTYPE}" "${MDDEVNAME}s2"
+"${SUDO_COMMAND}" gpart add -a "${FBPARTFSALIGN}" \
+    -t "${FBPARTFSTYPE}" "${MDDEVNAME}s2"
 "${SUDO_COMMAND}" makefs -B "${FBPARTFSENDIAN}" \
-    -f "${FBPARTMINFREE}" \
-    -t ffs -o label="${FBPARTFSLABEL}" -o version=2 \
+    -f "${FBPARTFSMINFREE}" \
+    -t ffs \
+    -o label="${FBPARTFSLABEL}" -o version=2 \
     -s "${FBPARTFSSIZE}" \
-    $(basename ${IMAGEFILE} .img).ufs ${DESTDIR}
+    $(basename ${IMAGEFILE} .img).ufs "${DESTDIR}"
 "${SUDO_COMMAND}" dd if=$(basename ${IMAGEFILE} .img).ufs \
     of="/dev/${MDDEVNAME}s2a" bs=1m
 "${SUDO_COMMAND}" mdconfig -d -u "${MDDEVNAME}"
 
-mkdir -p ${IMAGEDIR}
-mv -f "{$WORKDIR}/${IMAGEFILE}" "${IMAGEDIR}"
-echo "Your image is ready at "${IMAGEDIR}/${IMAGEFILE}"
+mkdir -p "${IMAGEDIR}"
+cp -pf "${WORKDIR}/${IMAGEFILE}" "${IMAGEDIR}"
+echo "Your image is ready at ${IMAGEDIR}/${IMAGEFILE}"
 echo "To write the image to a USB stick, do the following command:"
 echo "sudo dd if=${IMAGEDIR}/${IMAGEFILE} of=/dev/<your USB stick> bs=1m"
+
+rm -rf "${WORKDIR}"

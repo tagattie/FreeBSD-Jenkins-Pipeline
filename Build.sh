cdee07f772bf4d3f351dd47d18df3a92b0642eec
Hiroki Tagato <tagattie@gmail.com>
2017-10-23 13:41:38 +0900

    Tweak use of double quotations.

--- Build.sh
+++ Build.sh
@@ -3,6 +3,8 @@
 export  LANG=C
 export  PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
+SUDO_COMMAND=sudo
+
 # Comman-line format:
 # Build.sh srcDir objDirPrefix machArch cpuArch kernArgs destArgs additionalArgs buildTargets
 SRCDIR=${1}
@@ -24,7 +26,7 @@ NJOBS=$(sysctl hw.ncpu|awk '{print $NF}')
 MAKE_ENVS="MAKEOBJDIRPREFIX=${OBJDIRPREFIX}"
 MAKE_FLAGS="-DDB_FROM_SRC -DNO_FSCHG"
 MAKE_ARGS="TARGET=${ARCH_m} TARGET_ARCH=${ARCH_p} ${KERN_ARGS} ${DEST_ARGS} ${ADD_ARGS}"
-MAKE_TARGET="${TARGETS}"
+MAKE_TARGET=${TARGETS}
 
 if [ "${dryRunBuild}" == "true" ]; then
     DRYRUN_FLAG="-n"
@@ -39,8 +41,7 @@ for i in "$@"; do
     elif [ "${i}" == "installworld" ] || \
              [ "${i}" == "installkernel" ] || \
              [ "${i}" == "distribution" ]; then
-        SUDO_COMMAND="sudo"
-        FINAL_MAKE_FLAGS="${MAKE_FLAGS}"
+        FINAL_MAKE_FLAGS=${MAKE_FLAGS}
     fi
     BUILD_COMMAND="${SUDO_COMMAND} env ${MAKE_ENVS} make ${FINAL_MAKE_FLAGS} ${MAKE_ARGS} ${i}"
     (cd ${SRCDIR} && ${BUILD_COMMAND})
--- BuildImage-BEAGLEBONE.sh
+++ BuildImage-BEAGLEBONE.sh
@@ -5,7 +5,7 @@
 export  LANG=C
 export  PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
-SUDO_COMMAND="sudo"
+SUDO_COMMAND=sudo
 
 # Comman-line format:
 # BuildImage-BEAGLEBONE.sh buildName destDir imageDir hostname branchname archname confname
@@ -17,9 +17,9 @@ BRANCHNAME=${5}
 ARCHNAME=${6}
 CONFNAME=${7}
 
-WORKSPACE="$(pwd)"
-WORKDIR="${WORKSPACE}/work"
-IMAGEFILE="FreeBSD-${BRANCHNAME}-${ARCHNAME}-${HOSTNAME}-${BUILDNAME}.img"
+WORKSPACE=$(pwd)
+WORKDIR=${WORKSPACE}/work
+IMAGEFILE=FreeBSD-${BRANCHNAME}-${ARCHNAME}-${HOSTNAME}-${BUILDNAME}.img
 
 KiB=1024
 MiB=$((${KiB}*1024))
@@ -33,22 +33,22 @@ HEADSPERCYLINDER=255
 BYTESPERSECTOR=512
 STARTSECTOR=63
 # Parition scheme is MBR
-PARTSCHEME="MBR"
+PARTSCHEME=MBR
 # Boot partition
 BOOTPARTSIZE=$((16*${MiB}))
 BOOTPARTSIZEMB=$((${BOOTPARTSIZE}/${MiB}))
 BOOTPARTSECTORMARGIN=1
-BOOTPARTTYPE='!12'              # FAT32 (LBA-addressable)
-BOOTPARTLABEL="msdosboot"
-BOOTPARTFSTYPE="msdosfs"
-BOOTPARTFS="16"                 # FAT16
+BOOTPARTTYPE='!12'            # FAT32 (LBA-addressable)
+BOOTPARTLABEL=msdosboot
+BOOTPARTFSTYPE=msdosfs
+BOOTPARTFS=16                 # FAT16
 # FreeBSD partition
-FBPARTTYPE="freebsd"
-FBPARTSCHEME="BSD"
-FBPARTFSTYPE="freebsd-ufs"
+FBPARTTYPE=freebsd
+FBPARTSCHEME=BSD
+FBPARTFSTYPE=freebsd-ufs
 FBPARTFSALIGN=$((64*${KiB}))
-FBPARTFSENDIAN="big"
-FBPARTFSLABEL="rootfs"
+FBPARTFSENDIAN=big
+FBPARTFSLABEL=rootfs
 FBPARTFSMINFREE=$(bc -e "${IMGSIZE}/1024*0.5" -e quit | awk -F'.' '{print $1}')
 FBPARTFSMARGIN=$((1*${MiB}))
 FBPARTFSSIZE=$(((${IMGSIZEMB}-${BOOTPARTSIZEMB})*${MiB}-${FBPARTFSMARGIN}))
@@ -59,16 +59,16 @@ FBPARTFSSIZE=$(((${IMGSIZEMB}-${BOOTPARTSIZEMB})*${MiB}-${FBPARTFSMARGIN}))
 
 # Populate some files in advance of image building
 OVERLAYFILES="boot/loader.conf etc/fstab etc/rc.conf"
-OVERLAYFILEMODE="644"
-OVERLAYFILEOWNER="root"
-OVERLAYFILEGROUP="wheel"
-FIRSTBOOTSENTINEL="firstboot"
+OVERLAYFILEMODE=644
+OVERLAYFILEOWNER=root
+OVERLAYFILEGROUP=wheel
+FIRSTBOOTSENTINEL=firstboot
 for i in ${OVERLAYFILES}; do
-    "${SUDO_COMMAND}" install -c \
-        -m "${OVERLAYFILEMODE}" \
-        -o "${OVERLAYFILEOWNER}" \
-        -g "${OVERLAYFILEGROUP}" \
-        "${WORKSPACE}/overlays/${CONFNAME}/${i}" "${DESTDIR}/${i}"
+    ${SUDO_COMMAND} install -c \
+        -m ${OVERLAYFILEMODE} \
+        -o ${OVERLAYFILEOWNER} \
+        -g ${OVERLAYFILEGROUP} \
+        ${WORKSPACE}/overlays/${CONFNAME}/${i} ${DESTDIR}/${i}
 done
 ${SUDO_COMMAND} install -d \
     -o ${OVERLAYFILEOWNER} \
@@ -78,79 +78,78 @@ ${SUDO_COMMAND} install -d \
 # "${SUDO_COMMAND}" truncate -s "${SWAPFILESIZE}" "${DESTDIR}/${SWAPFILENAME}"
 # "${SUDO_COMMAND}" chmod 600 "${DESTDIR}/${SWAPFILENAME}"
 ###
-"${SUDO_COMMAND}" touch "${DESTDIR}/${FIRSTBOOTSENTINEL}"
+${SUDO_COMMAND} touch ${DESTDIR}/${FIRSTBOOTSENTINEL}
 
-mkdir -p "${WORKDIR}"
+mkdir -p ${WORKDIR}
 
 # Create image file and mount it as memory disk
-truncate -s "${IMGSIZE}" "${WORKDIR}/${IMAGEFILE}"
+truncate -s ${IMGSIZE} ${WORKDIR}/${IMAGEFILE}
 MDDEVNAME=$(${SUDO_COMMAND} mdconfig -a -t vnode \
     -x ${SECTORSPERTRACK} -y ${HEADSPERCYLINDER} -f ${WORKDIR}/${IMAGEFILE})
-echo "Memory disk device name is ${MDDEVNAME}"
+echo "Memory disk device name is ${MDDEVNAME}."
 
 # Create partition scheme
-"${SUDO_COMMAND}" gpart create -s "${PARTSCHEME}" "${MDDEVNAME}"
+${SUDO_COMMAND} gpart create -s ${PARTSCHEME} ${MDDEVNAME}
 # Create FAT32(LBA-addressable) partition for boot (1st slice)
-"${SUDO_COMMAND}" gpart add -a "${SECTORSPERTRACK}" \
-    -b "${STARTSECTOR}" \
+${SUDO_COMMAND} gpart add -a ${SECTORSPERTRACK} \
+    -b ${STARTSECTOR} \
     -s $((${BOOTPARTSIZE}/${BYTESPERSECTOR}-${BOOTPARTSECTORMARGIN})) \
-    -t "${BOOTPARTTYPE}" "${MDDEVNAME}"
+    -t ${BOOTPARTTYPE} ${MDDEVNAME}
 # Make the slice (boot slice) active (bootable)
-"${SUDO_COMMAND}" gpart set -a active \
-    -i 1 "${MDDEVNAME}"
+${SUDO_COMMAND} gpart set -a active -i 1 ${MDDEVNAME}
 # Create FAT16 filesystem in the boot slice
-"${SUDO_COMMAND}" newfs_msdos -L "${BOOTPARTLABEL}" \
-    -F "${BOOTPARTFS}" "/dev/${MDDEVNAME}s1"
+${SUDO_COMMAND} newfs_msdos -L ${BOOTPARTLABEL} \
+    -F ${BOOTPARTFS} /dev/${MDDEVNAME}s1
 # Mount the FAT16 filesystem and copy necessary files onto it
-mkdir -p "${WORKDIR}/${BOOTPARTLABEL}"
-"${SUDO_COMMAND}" mount -t "${BOOTPARTFSTYPE}" \
-    -l "/dev/${MDDEVNAME}s1" "${WORKDIR}/${BOOTPARTLABEL}"
+mkdir -p ${WORKDIR}/${BOOTPARTLABEL}
+${SUDO_COMMAND} mount -t ${BOOTPARTFSTYPE} \
+    -l /dev/${MDDEVNAME}s1 ${WORKDIR}/${BOOTPARTLABEL}
 
-UBOOTBASEDIR="/usr/local/share/u-boot/u-boot-beaglebone"
+UBOOTBASEDIR=/usr/local/share/u-boot/u-boot-beaglebone
 UBOOTFILES="MLO u-boot.img"
-UBLDRBASEDIR="${DESTDIR}/boot"
+UBLDRBASEDIR=${DESTDIR}/boot
 UBLDRFILES="ubldr ubldr.bin"
 for i in ${UBOOTFILES}; do
-    "${SUDO_COMMAND}" install -c \
-        "${UBOOTBASEDIR}/${i}" "${WORKDIR}/${BOOTPARTLABEL}/${i}"
+    ${SUDO_COMMAND} install -c \
+        ${UBOOTBASEDIR}/${i} ${WORKDIR}/${BOOTPARTLABEL}/${i}
 done
 for i in ${UBLDRFILES}; do
-    "${SUDO_COMMAND}" install -c \
-        "${UBLDRBASEDIR}/${i}" "${WORKDIR}/${BOOTPARTLABEL}/${i}"
+    ${SUDO_COMMAND} install -c \
+        ${UBLDRBASEDIR}/${i} ${WORKDIR}/${BOOTPARTLABEL}/${i}
 done
 
-"${SUDO_COMMAND}" umount "${WORKDIR}/${BOOTPARTLABEL}"
-rmdir "${WORKDIR}/${BOOTPARTLABEL}"
+${SUDO_COMMAND} umount ${WORKDIR}/${BOOTPARTLABEL}
+rmdir ${WORKDIR}/${BOOTPARTLABEL}
 
 # Create FreeBSD partition (2nd slice)
-"${SUDO_COMMAND}" gpart add -t "${FBPARTTYPE}" "${MDDEVNAME}"
+${SUDO_COMMAND} gpart add -t ${FBPARTTYPE} ${MDDEVNAME}
 # Set partition scheme of the FreeBSD slice
-"${SUDO_COMMAND}" gpart create -s "${FBPARTSCHEME}" "${MDDEVNAME}s2"
+${SUDO_COMMAND} gpart create -s ${FBPARTSCHEME} ${MDDEVNAME}s2
 # Add freebsd-ufs partition (make alignment to 64KiB)
-"${SUDO_COMMAND}" gpart add -a "${FBPARTFSALIGN}" \
-    -t "${FBPARTFSTYPE}" "${MDDEVNAME}s2"
+${SUDO_COMMAND} gpart add -a ${FBPARTFSALIGN} \
+    -t ${FBPARTFSTYPE} ${MDDEVNAME}s2
 # Create UFS filesystem in the FreeBSD slice
-"${SUDO_COMMAND}" newfs -U -j -t -L "${FBPARTFSLABEL}" "/dev/${MDDEVNAME}s2a"
+${SUDO_COMMAND} newfs -U -j -t -L ${FBPARTFSLABEL} /dev/${MDDEVNAME}s2a
 # Mount the UFS filesystem and copy distribution onto it
-mkdir -p "${WORKDIR}/${FBPARTFSLABEL}"
-"${SUDO_COMMAND}" mount -t ufs "/dev/${MDDEVNAME}s2a" "${WORKDIR}/${FBPARTFSLABEL}"
+mkdir -p ${WORKDIR}/${FBPARTFSLABEL}
+${SUDO_COMMAND} mount -t ufs /dev/${MDDEVNAME}s2a ${WORKDIR}/${FBPARTFSLABEL}
 
-(cd "${DESTDIR}" && \
-    "${SUDO_COMMAND}" find . -print -depth | \
-    "${SUDO_COMMAND}" cpio -padm "${WORKDIR}/${FBPARTFSLABEL}")
-(cd "${WORKDIR}/${FBPARTFSLABEL}/boot/dtb" && \
-    "${SUDO_COMMAND}" ln beaglebone.dtb am335x-bone.dtb &&
-    "${SUDO_COMMAND}" ln beaglebone-black.dtb am335x-boneblack.dtb)
+(cd ${DESTDIR} && \
+    ${SUDO_COMMAND} find . -print -depth | \
+    ${SUDO_COMMAND} cpio -padm ${WORKDIR}/${FBPARTFSLABEL})
+(cd ${WORKDIR}/${FBPARTFSLABEL}/boot/dtb && \
+    ${SUDO_COMMAND} ln -f beaglebone.dtb am335x-bone.dtb &&
+    ${SUDO_COMMAND} ln -f beaglebone-black.dtb am335x-boneblack.dtb)
 
-"${SUDO_COMMAND}" umount "${WORKDIR}/${FBPARTFSLABEL}"
-"${SUDO_COMMAND}" mdconfig -d -u "${MDDEVNAME}"
+${SUDO_COMMAND} umount ${WORKDIR}/${FBPARTFSLABEL}
+${SUDO_COMMAND} mdconfig -d -u ${MDDEVNAME}
 
 # Copy image file to specified directory
-mkdir -p "${IMAGEDIR}"
-cp -pf "${WORKDIR}/${IMAGEFILE}" "${IMAGEDIR}"
+mkdir -p ${IMAGEDIR}
+cp -pf ${WORKDIR}/${IMAGEFILE} ${IMAGEDIR}
 
-echo "Your image is ready at ${IMAGEDIR}/${IMAGEFILE}"
-echo "To write the image to a USB stick, do the following command:"
-echo "sudo dd if=${IMAGEDIR}/${IMAGEFILE} of=/dev/<your USB stick> bs=1m"
+echo "Your image is ready at ${IMAGEDIR}/${IMAGEFILE}."
+echo "To write the image to an SD card, do the following command:"
+echo "sudo dd if=${IMAGEDIR}/${IMAGEFILE} of=/dev/<your SD card> bs=1m"
 
-rm -rf "${WORKDIR}"
+rm -rf ${WORKDIR}
